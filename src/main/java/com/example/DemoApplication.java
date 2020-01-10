b8bfafe3fd20193f1e4ad97dac3f57f1c5c926cb
hantao <hantaoemail@sina.cn>
2020-01-10 16:11:49 +0800

    修改

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo111;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Demo111Application {
+public class DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Demo111Application.class, args);
+        SpringApplication.run(DemoApplication.class, args);
     }
 
 }
---
+++ src/main/java/com/example/dao/DaoOne.java
@@ -0,0 +1,10 @@
+package com.example.dao;
+
+/**
+ * description: DaoOne
+ * date: 2020/1/10 16:10
+ * author: hantao
+ * version: 1.0
+ */
+public class DaoOne {
+}
